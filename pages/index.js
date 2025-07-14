dd3064375097abae12433a785ba7558fd0bbbed7
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 06:44:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,29 @@
-ngyu
+import Header from '../components/Header';
+import VideoPlayer from '../components/VideoPlayer';
+import TelegramPopup from '../components/TelegramPopup';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main className="container mx-auto py-6 px-4">
+        <VideoPlayer videoId="dQw4w9WgXcQ" />
+      </main>
+      <TelegramPopup />
+      {/* Prefetch Script */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            if('serviceWorker' in navigator) {
+              navigator.serviceWorker.register('/service-worker.js');
+            }
+            // Prefetch next video
+            const nextVideoId = 'next-video-id';
+            const videoUrl = '/videos/' + nextVideoId;
+            fetch(videoUrl, { priority: 'low' });
+          `,
+        }}
+      />
+    </div>
+  );
+}
