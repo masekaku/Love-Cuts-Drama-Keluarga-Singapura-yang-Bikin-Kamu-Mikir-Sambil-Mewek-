84307b3ee61a548659f7a6e26402e9d429271a30
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 06:44:34 +0700

    Update CacheHandler.js

--- components/CacheHandler.js
+++ components/CacheHandler.js
@@ -1,1 +1,19 @@
-tuu
+import { getCachedVideo, setCachedVideo } from '../utils/cache';
+
+export async function getServerSideProps({ params }) {
+  const videoId = params.id;
+  let videoData = getCachedVideo(videoId);
+
+  if (!videoData) {
+    // Simulate API fetch
+    videoData = await fetchVideoData(videoId);
+    setCachedVideo(videoId, videoData);
+  }
+
+  return { props: { videoData } };
+}
+
+async function fetchVideoData(id) {
+  // Actual API call would go here
+  return { id, title: `Video ${id}`, description: '...' };
+}
