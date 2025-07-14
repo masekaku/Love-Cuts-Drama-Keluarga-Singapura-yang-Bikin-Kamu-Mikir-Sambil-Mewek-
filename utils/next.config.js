2b14bc66815dc9a8079a3811d511f0c2b2f524f2
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 06:47:49 +0700

    Update next.config.js

--- utils/next.config.js
+++ utils/next.config.js
@@ -1,1 +1,13 @@
-giitd
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ['i.ytimg.com'],
+  },
+});
