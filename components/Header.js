58efd927696f33c03a575306ee8c6c03a7594732
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 06:46:54 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,9 @@
-d
+export default function Header() {
+  return (
+    <header className="bg-black text-white p-4 shadow-lg">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold">VideoStream</h1>
+      </div>
+    </header>
+  );
+}
