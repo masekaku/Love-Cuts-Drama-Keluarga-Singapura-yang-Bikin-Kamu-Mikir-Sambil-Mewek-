7fc4ddc77f0987071a94463640bcbe1819b535e6
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:20:19 +0700

    Update next.config.js

--- video-website/next.config.js
+++ video-website/next.config.js
@@ -1,1 +1,21 @@
-tess
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.videy.co'],
+  },
+  experimental: {
+    nextScriptWorkers: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/mock-videos.json',
+        destination: '/api/videos',
+      }
+    ]
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+  }
+};
