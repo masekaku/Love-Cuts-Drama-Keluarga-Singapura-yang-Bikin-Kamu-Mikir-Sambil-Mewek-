602d301230321c4ffe899c123e163544d839fb5a
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:19:22 +0700

    Update _document.js

--- video-website/pages/_document.js
+++ video-website/pages/_document.js
@@ -1,1 +1,19 @@
-tesss
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://cdn.videy.co" />
+        <link rel="dns-prefetch" href="https://cdn.videy.co" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
