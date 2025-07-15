3c35bc9598a4618dd70261713d02fd05e003d26c
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:22:54 +0700

    Delete utils directory

--- utils/cache.js
+++
@@ -1,9 +0,0 @@
-import LRU from 'lru-cache';
-
-const videoCache = new LRU({
-  max: 100, // Max cached items
-  maxAge: 1000 * 60 * 15, // 15 minutes
-});
-
-export const getCachedVideo = (id) => videoCache.get(id);
-export const setCachedVideo = (id, data) => videoCache.set(id, data);
