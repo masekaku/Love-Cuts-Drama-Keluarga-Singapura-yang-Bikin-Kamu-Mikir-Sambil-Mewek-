941b1d5cd8a3ddf961b569415ec894b85d98bcaa
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:23:16 +0700

    Delete components directory

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-black text-white p-4 shadow-lg">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">VideoStream</h1>
-      </div>
-    </header>
-  );
-}
