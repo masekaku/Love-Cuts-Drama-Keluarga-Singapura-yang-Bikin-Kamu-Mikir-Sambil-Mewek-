c308245eaff7479a5b0dd44da79ca8c5b206c45b
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:19:41 +0700

    Update cache.js

--- video-website/utils/cache.js
+++ video-website/utils/cache.js
@@ -1,1 +1,19 @@
-tess
+import LRU from 'lru-cache';
+
+// Cache for video list
+const videoListCache = new LRU({
+  max: 1,
+  maxAge: 1000 * 60 * 5, // 5 minutes
+});
+
+// Cache for individual videos
+const videoCache = new LRU({
+  max: 100,
+  maxAge: 1000 * 60 * 60, // 1 hour
+});
+
+export const getCachedVideos = () => videoListCache.get('latest');
+export const setCachedVideos = (videos) => videoListCache.set('latest', videos);
+
+export const getCachedVideo = (id) => videoCache.get(id);
+export const setCachedVideo = (id, data) => videoCache.set(id, data);
