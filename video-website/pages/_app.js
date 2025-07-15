43246ea94dfd4d3f754c8f94e0c2201cf6666d91
masekaku <aguslistiyanto27@gmail.com>
2025-07-15 07:19:05 +0700

    Update _app.js

--- video-website/pages/_app.js
+++ video-website/pages/_app.js
@@ -1,1 +1,15 @@
-tesss
+import '@/styles/globals.css';
+import Header from '@/components/Header';
+import TelegramPopup from '@/components/TelegramPopup';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <TelegramPopup />
+    </div>
+  );
+}
